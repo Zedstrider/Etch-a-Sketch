e4dbd12d3b2085422892172c155398cf00b9942d
Zedstrider <206158702+Zedstrider@users.noreply.github.com>
2025-08-06 18:42:21 +0530

    Create button to change grid size

--- app.js
+++ app.js
@@ -10,7 +10,18 @@ gridcontainer.addEventListener("mouseover",(event)=>{
     event.target.style.background="blue"
 });
 
-
+const btn=document.querySelector("button");
+btn.addEventListener("click",()=>{
+    const input=prompt("Enter size(Below 100)");
+    gridcontainer.replaceChildren();
+    const size=Number(input)
+    for(let i=1;i<size*size;i++)
+{
+const griditem=document.createElement("div");
+griditem.classList.add("grid-item")
+gridcontainer.appendChild(griditem)
+}
+})
 
 
 
